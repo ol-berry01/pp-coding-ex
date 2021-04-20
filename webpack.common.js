be24ed89e9e42200ffbe7ed8f9f4c000f8f819f7
ol-berry01 <info@littlemonsterdigital.co.uk>
2021-04-20 10:01:39 +0100

    Header
    header section:
    - add markup for header & nav
    
    _resets.scss:
    - declared initial reset values
    
    _variables.scss:
    - declared initial project colour variables
    - declared initial project font variables
    
    _fonts.scss:
    - imported 'Avallon' font
    - imported 'Merlod Norme Semi Bold'
    
    _buttons.scss:
    - setup initial buttons style rules
    
    _header.scss:
    - add style rules for mobile format
    - add media queries for other screen sizes
    
    webpack.coomon.js:
    - setup module rules for images
    - setup module rules for fonts
    
    scripts.js:
    - imported bootstrap
    
    images:
    - added navbar background
    - added las iguanas logo
    
    fonts:
    - added Avallon (woff/woff2)
    - added Merlod Norme Semi Bold (woff/woff2)
    
    Dependancies:
    - installed file-loader
    
    Update gitignore:
    - added dist folder

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
@@ -18,6 +19,30 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: {
+          loader: "file-loader",
+          options: {
+            entry: path.resolve(__dirname, "inc/img"),
+            name: "[name].[ext]",
+            outputPath: "../img/"
+          }
+        }
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              entry: path.resolve(__dirname, "inc/fonts"),
+              name: "[name].[ext]",
+              outputPath: "../fonts/"
+            }
+          }
+        ]
       }
     ]
   },
